a6245132f0405b2113b8300aa157ee9abf29bfd8
Laurence279 <lnunn279@gmail.com>
2022-04-11 21:06:58 +0100

    Add handler for fileupload

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var cors = require('cors');
+const { urlencoded } = require('express');
 require('dotenv').config()
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
 app.use(cors());
+app.use(urlencoded({extended: true}))
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post("/api/fileanalyse", upload.single('upfile'), function (req, res) {
+  console.log(req.file)
+  const {originalname, mimetype, size} = req.file;
+
+  res.json({name: originalname, type: mimetype, sizeInBytes: size})
+})
 
 
 
